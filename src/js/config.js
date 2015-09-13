5387cdfb3532b870cce260bc0441f3f89fe06a9b
Lukas Juhas <hello@lukasjuhas.com>
2015-09-13 20:40:12 +0100

    add sass structure, tweak some things, config file

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+const config = {
+  twitterConsumerKey: 'XgAr6cnh2tFfEjxlo3SD7wLJk',
+  twitterConsumerSecret: 'nkBoCFcWWLHRF2IX8hnaCQS2JWWvz3fdJnpy8tc6EYjHi2dUjt',
+};
+module.exports = config;
--- src/js/views/index.jsx
+++ src/js/views/index.jsx
@@ -1,9 +1,9 @@
-var React = require('react');
+const React = require('react');
 
-var App = React.createClass({
+const App = React.createClass({
   render: function() {
     return (
-      <h1>Hello world!</h1>
+      <h1>Lukas Juhas</h1>
     );
   },
 
